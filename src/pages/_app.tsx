f3dd994e0e16e95385123b669a3a64d48ce1b03c
katisokko <113367535+katisokko@users.noreply.github.com>
2022-11-09 16:11:12 +0200

    matkaklubi

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,15 @@
+//import { Montserrat } from '@next/font/google'
+
 import '../styles/globals.css'
 
+//const montserrat = Montserrat()
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    //<main className={montserrat.className}>
+      <Component {...pageProps} />
+    //</main>
+  )
 }
 
 export default MyApp
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,93 +1,50 @@
 import { NextPage } from "next";
 import Head from "next/head";
-import { useMemo, useState } from "react";
-import { createTodo, deleteTodo, toggleTodo, useTodos } from "../api";
+import Image from 'next/image'
 import styles from "../styles/Home.module.css";
-import { Todo } from "../types";
 
-export const TodoList: React.FC = () => {
-  const { data: todos, error } = useTodos();
-
-  if (error != null) return <div>Error loading todos...</div>;
-  if (todos == null) return <div>Loading...</div>;
-
-  if (todos.length === 0) {
-    return <div className={styles.emptyState}>Try adding a todo ☝️️</div>;
-  }
-
+const Pais = () => {
   return (
-    <ul className={styles.todoList}>
-      {todos.map(todo => (
-        <TodoItem todo={todo} />
-      ))}
-    </ul>
-  );
-};
-
-const TodoItem: React.FC<{ todo: Todo }> = ({ todo }) => (
-  <li className={styles.todo}>
-    <label
-      className={`${styles.label} ${todo.completed ? styles.checked : ""}`}
-    >
-      <input
-        type="checkbox"
-        checked={todo.completed}
-        className={`${styles.checkbox}`}
-        onChange={() => toggleTodo(todo)}
-      />
-      {todo.text}
-    </label>
-
-    <button className={styles.deleteButton} onClick={() => deleteTodo(todo.id)}>
-      ✕
-    </button>
-  </li>
-);
-
-const AddTodoInput = () => {
-  const [text, setText] = useState("");
-
-  return (
-    <form
-      onSubmit={async e => {
-        e.preventDefault();
-        createTodo(text);
-        setText("");
-      }}
-      className={styles.addTodo}
-    >
-      <input
-        className={styles.input}
-        placeholder="Buy some milk"
-        value={text}
-        onChange={e => setText(e.target.value)}
-      />
-      <button className={styles.addButton}>Add</button>
-    </form>
-  );
-};
+    <div className={styles.pais}>
+        <nav className={styles.paisNav}>
+            <a href="index.html" className={styles.logo}>
+                {/* <img src="assets/logo.png" alt="Matkaklubi logo" className="logoPilt">*/}
+                <div className="logoTekst">
+                    <span>HIKING</span>
+                    <span>CLUB</span>
+                </div>
+            </a>
+            <div className="paremPool">
+                <a href="index.html" className={styles.paremPoolLink}>HOME</a>
+                <a href="index.html" className={styles.paremPoolLink}>UPCOMING TREKS</a>
+                <a href="index.html" className={styles.paremPoolLink}>TREK FOR FAMILY</a>
+                <a href="index.html" className={styles.paremPoolLink}>CONTACT US</a>
+            </div>
+        </nav>
+        <div className={styles.alumine}>
+            <div className={styles.sisu}>
+                <h1 className={'${styles.pealkirjad} $styles.paisAlumineSisuH1}'}>CLIMB FOR BETTER</h1>
+                <h2 className={'${styles.pealkirjad} $styles.paisAlumineSisuH2}'}>GET OUT NOW</h2>
+                <h4 className={'${styles.pealkirjad} $styles.paisAlumineSisuH4}'}>JOIN US FOR THE WEEKEND TO CLIMB<br>
+                THE HIGHEST PEAK IN ENGLAND</h4>
+                <a href="upcoming.html"> 
+                <button className={styles.nupp}>VIEW UPCOMING TREKS</button>
+                </a>
+            </div>
+            <div className="pilt"></div>   
+        </div>
+    </div>
+  )
+}
 
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Railway NextJS Prisma</title>
+        <title>Matkaklubi</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <header className={styles.header}>
-        <h1 className={styles.title}>Todos</h1>
-        <h2 className={styles.desc}>
-          NextJS app connected to Postgres using Prisma and hosted on{" "}
-          <a href="https://railway.app">Railway</a>
-        </h2>
-      </header>
-
-      <main className={styles.main}>
-        <AddTodoInput />
-
-        <TodoList />
-      </main>
+      <Pais />
     </div>
   );
 };
