c9971845636e3a7f398ac42a2eae062c4e4bce0e
katisokko <113367535+katisokko@users.noreply.github.com>
2022-11-14 15:55:29 +0200

    matkaklubi

---
+++ src/pages/kontakt.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import Image from "next/image"
+import Navigatsioon from "../components/Navigatsioon";
+import styles from "../styles/Kontakt.module.css";
+
+
+
+export default () => {
+    return (
+        <div className={styles.pais}>
+            <Navigatsioon />
+        </div>
+    )
+}
